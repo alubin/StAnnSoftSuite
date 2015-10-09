78737c4f4507a1730535cabb76b9389ccbd4a6b7
Andy Lubin <al48@students.uwf.edu>
2015-10-09 16:46:32 -0500

    Inital Commit for Comp Sci Project

---
+++ CFC/src/cfc/DataHandler.java
@@ -0,0 +1,14 @@
+package cfc;
+
+import java.util.ArrayList;
+
+public class DataHandler {
+
+	private ArrayList<DioceseData> users;
+
+	public static ArrayList<DioceseData> extractInfo()
+	{
+		return null;
+	}
+
+}
---
+++ CFC/src/cfc/DioceseData.java
@@ -0,0 +1,5 @@
+package cfc;
+
+public class DioceseData {
+
+}
