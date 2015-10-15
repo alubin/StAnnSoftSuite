3f44312c3e9fe605f9802b34a07e9f5d48214c94
AL <andylubin@gmail.com>
2015-10-15 00:37:48 -0500

    Work on CFC Access Panel

---
+++ CFC/src/cfc/CFCAccessPanel.java
@@ -0,0 +1,21 @@
+package cfc;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class CFCAccessPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public CFCAccessPanel()
+	{
+		setName("Enter Access File");
+		setLayout(new BorderLayout());
+	}
+
+}
