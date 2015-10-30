a2a310dfdf08ac291c4c657976fd8d2bbf4f7d99
Andy Lubin <al48@students.uwf.edu>
2015-10-30 16:16:06 -0500

    Began Development for the reading of the data from the Access Database.

--- CFC/src/ccf/AccessReader.java
+++ CFC/src/ccf/AccessReader.java
@@ -4,34 +4,48 @@ import java.io.File;
 import java.io.IOException;
 
 import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Row;
 import com.healthmarketscience.jackcess.Table;
 
 public class AccessReader {
-	
+
 	private String fileName;
-	
+
 	public AccessReader()
 	{
-		
+
 	}
 	public AccessReader(String accessFile)
 	{
 		this();
 		fileName = accessFile;
 	}
-	
+
 	public static Table readTable(String fileName, String tableName) throws IOException
 	{
 		Table table = DatabaseBuilder.open(new File(fileName)).getTable(tableName);
 		return table;
 	}
-	
+
 	private void storeInfo()
 	{
-		
+
 	}
-	
-	
-	
+	public void displayTable(String tableName) {
+		try {
+			Table table = DatabaseBuilder.open(new File(fileName)).getTable(tableName);
+			for(Row row : table)
+			{
+				System.out.println(row);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+
+
 
 }
--- CFC/src/ccf/CCFActionListener.java
+++ CFC/src/ccf/CCFActionListener.java
@@ -12,6 +12,7 @@ public class CCFActionListener extends Convert implements ActionListener {
 		//TODO: Put in code that will open a file browser and convert selected file.
 		System.out.println("CCF");
 		System.out.println("File Path = " + getFilePath());
+		new AccessReader(getFilePath()).displayTable("Test");
 
 	}
 
