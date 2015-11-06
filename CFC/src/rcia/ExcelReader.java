610ad15996160ed1569302d0c2896da1476bc62b
Andy Lubin <al48@students.uwf.edu>
2015-11-06 16:27:01 -0600

    -Added Functionality to better display the excel data

--- CFC/src/rcia/ExcelReader.java
+++ CFC/src/rcia/ExcelReader.java
@@ -3,6 +3,7 @@ package rcia;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.JTable;
@@ -45,24 +46,26 @@ public class ExcelReader {
 
 
 	public void displayTable() {
+		ArrayList<ArrayList<String>> rowArray = new ArrayList<ArrayList<String>>();
 		try
         {
             FileInputStream file = new FileInputStream(new File(fileName));
- 
+
             //Create Workbook instance holding reference to .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
- 
+
             //Get first/desired sheet from the workbook
             XSSFSheet sheet = workbook.getSheetAt(0);
- 
+
             //Iterate through each rows one by one
             Iterator<Row> rowIterator = sheet.iterator();
             while (rowIterator.hasNext())
             {
+            	ArrayList<String> cellArray = new ArrayList<String>();
                 Row row = rowIterator.next();
                 //For each row, iterate through all the columns
                 Iterator<Cell> cellIterator = row.cellIterator();
-                 
+
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
@@ -70,24 +73,32 @@ public class ExcelReader {
                     switch (cell.getCellType())
                     {
                         case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "t");
+//                            System.out.print(cell.getNumericCellValue());
+                            cellArray.add(String.valueOf(cell.getNumericCellValue()));
                             break;
                         case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "t");
+//                            System.out.print(cell.getStringCellValue());
+                            cellArray.add(cell.getStringCellValue());
                             break;
                     }
-                    System.out.println("");
+//                    System.out.println("");
                 }
-                System.out.println("\n");
+                rowArray.add(cellArray);
+//                System.out.println("\n");
             }
             file.close();
+
+            for(ArrayList<String> rowValue: rowArray)
+            {
+            	System.out.println("Row = " + rowValue);
+            }
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-		
-		
+
+
 
 
 	}
