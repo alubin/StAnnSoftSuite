5656e0c7348aa0a79e79252f287580b762f652d4
Taylor Maines <trm28@students.uwf.edu>
2015-11-07 13:02:13 -0600

    Added INSERT prepared statement functionality to add import from access database to SQL database

--- CFC/src/database/DbWorker.java
+++ CFC/src/database/DbWorker.java
@@ -1,7 +1,13 @@
 package database;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.util.ArrayList;
+
+import org.apache.poi.hwpf.usermodel.DateAndTime;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import rcia.RciaData;
 import ccf.CCFData;
@@ -45,14 +51,43 @@ public class DbWorker {
 	}
 
 	public void storeCCF(ArrayList<CCFData> data){
+		
 		System.out.println("Writing to CCF Database.");
 		//TODO: Extract the data from the array and store the data.
 		for(CCFData results : data)
 		{
+			try{
+				PreparedStatement pStmt = con1.prepareStatement("INSERT INTO `ccf`.`parishdata`(`ID`,`StuID`,`lblParishID`,"
+						+ "`DOB1`,`StuFunction`,`StuStatus`,`StuLnm`,`StuFnm`,`Student`,`ClsOfferedID`,`HrsCredited`,"
+							+ "`ClsDate`,`CrsTitle`,`Instructor`,`Expr1`,`CrsLevel`) "
+								+ "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+				
+				pStmt.setInt(1, results.getId());
+				pStmt.setDouble(2, results.getStudentId());
+				pStmt.setString(3, results.getParishId());
+				pStmt.setString(4, results.getDob());
+				pStmt.setString(5, results.getFunction());
+				pStmt.setString(6, results.getStatus());
+				pStmt.setString(7, results.getLastName());
+				pStmt.setString(8, results.getFirstName());
+				pStmt.setString(9, results.getName());
+				pStmt.setDouble(10, results.getClassOffered());
+				pStmt.setDouble(11, results.getHoursCredited());
+				pStmt.setDate(12, (Date)results.getClassDate());
+				pStmt.setString(13, results.getCourseTitle());
+				pStmt.setString(14, results.getInstructor());
+				pStmt.setString(15, results.getExperienced());
+				pStmt.setString(16, results.getCourseLevel());
+				
+				pStmt.executeUpdate();
+				pStmt.close();
+			}
+			catch (SQLException e) {
+				
+			}
+			results.getId();
 			System.out.println(results);
 		}
-
-
 	}
 
 	public void storeRCIA(ArrayList<RciaData> data){
