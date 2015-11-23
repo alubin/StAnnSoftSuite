0e31465cc5d324f35db35e86eacf9578e6e7fb1a
AL <andylubin@gmail.com>
2015-11-23 03:14:10 -0500

    Started the template for the RCIA Admin Panels

---
+++ CFC/src/rcia/admin/panels/MemberInfo.java
@@ -0,0 +1,22 @@
+package rcia.admin.panels;
+
+/* 
+ * Birth Name
+ * First
+ * Middle
+ * Last
+ * Address
+ * City
+ * State
+ * Zip
+ * Email
+ * Phone
+ * Age
+ * DOB
+ * Gender
+ * Role
+ * Saint
+ */
+public class MemberInfo {
+
+}
