2c4bb8d29ff7067d33a82a72e760dc0c319aed7d
Taylor Maines <trm28@students.uwf.edu>
2015-12-02 20:14:57 -0600

    Fixed Certificate duplicates, and sizing of buttons in PrintSheet

--- CFC/src/print/PrintSheet.java
+++ CFC/src/print/PrintSheet.java
@@ -1,16 +1,12 @@
 package print;
 
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Enumeration;
+
 import java.util.HashMap;
 
 import javax.swing.JDialog;
@@ -40,10 +36,11 @@ import database.DatabaseStore;
 import database.DbConnectErrorDialog;
 import database.DbWorker;
 
-
 import java.awt.Desktop;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
 
 public class PrintSheet extends JDialog {
 
@@ -64,7 +61,7 @@ private JPanel contentPane;
 
 		setAlwaysOnTop(true);
 
-		setBounds(100, 100, 180, 125);
+		setBounds(100, 100, 200, 100);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -72,86 +69,87 @@ private JPanel contentPane;
 		URL url = getClass().getResource("/images/loading_bar_small.gif");
 		Icon img = new ImageIcon(url);
 		lbl = new JLabel(img);
-		lbl.setBounds(0, 70, 164, 17);
-		
-		contentPane.setLayout(null);
+		lbl.setBounds(5, 51, 154, 17);
 		lbl.setVisible(false);
-		getContentPane().add(lbl);
+		contentPane.add(lbl);
 		
-		JLabel lblPrintSignSheet = new JLabel("Print Sign-In Sheet: ");
-		lblPrintSignSheet.setBounds(10, 11, 128, 14);
-		contentPane.add(lblPrintSignSheet);
-
 		JButton acceptButton = new JButton("Accept");
-		acceptButton.setBounds(10, 36, 65, 23);
+		acceptButton.setBounds(10, 23, 77, 23);
 		
 		acceptButton.addActionListener(new ActionListener() {
-			
+					
 			public void actionPerformed(ActionEvent e) {
+						
+						lbl.setVisible(true);
+						contentPane.repaint();
+						contentPane.revalidate();
+						
+						try
+						{
+							db = new DbWorker(DatabaseStore.getAddress(), DatabaseStore.getPort(),
+									DatabaseStore.getUserName(), DatabaseStore.getPassword());
+							con = db.getConnection();
+
+						}
+						catch (Exception e1)
+						{
+							e1.printStackTrace();
+							new DbConnectErrorDialog().setVisible(true);
+						}
+
+
+						String OUT_PUT = "./sign_in_sheet.xlsx";
+
+						String REPORT = "/certificates/Signing_List.jrxml";
+
+						HashMap<String, Object> map = new HashMap<>();
+						//map.put("FirstName",firstName);
+
+						try
+						{
+							JasperDesign jd  = JRXmlLoader.load(getClass().getResourceAsStream(REPORT));
+					        JasperReport jr = JasperCompileManager.compileReport(jd);
+					        JasperPrint jp = JasperFillManager.fillReport(jr, map, con);
+					        JRXlsxExporter export = new JRXlsxExporter();
+						    export.setExporterInput(new SimpleExporterInput(jp));
+						    export.setExporterOutput(new SimpleOutputStreamExporterOutput(new File(OUT_PUT)));
+						    SimpleXlsxReportConfiguration config = new SimpleXlsxReportConfiguration();
+						    export.setConfiguration(config);
+						    export.exportReport();
+						    JOptionPane.showMessageDialog(mainGui, "<html>The sign-in sheet was successfully created</html>");
+
+						    lbl.setVisible(false);
+						    setVisible(false);
+						    lbl.repaint();
+						    
+						    //Opens file after completed export
+						    OpenFile(OUT_PUT);
+					    } catch (JRException ex) {
+					        ex.printStackTrace();
+					        lbl.setVisible(false);
+					        lbl.repaint();
+					        JOptionPane.showMessageDialog(mainGui, "<html>The sign-in sheet could not be created due to an error:<br>" + ex + "</html>", "Error", JOptionPane.ERROR_MESSAGE);
+					    }
+
+
+					}
+				});
+				contentPane.setLayout(null);
 				
-				lbl.setVisible(true);
-				lbl.repaint();
-				
-				try
-				{
-					db = new DbWorker(DatabaseStore.getAddress(), DatabaseStore.getPort(),
-							DatabaseStore.getUserName(), DatabaseStore.getPassword());
-					con = db.getConnection();
-
-				}
-				catch (Exception e1)
-				{
-					e1.printStackTrace();
-					new DbConnectErrorDialog().setVisible(true);
-				}
-
-
-				String OUT_PUT = "./sign_in_sheet.xlsx";
-
-				String REPORT = "/certificates/Signing_List.jrxml";
-
-				HashMap<String, Object> map = new HashMap<>();
-				//map.put("FirstName",firstName);
-
-				try
-				{
-					JasperDesign jd  = JRXmlLoader.load(getClass().getResourceAsStream(REPORT));
-			        JasperReport jr = JasperCompileManager.compileReport(jd);
-			        JasperPrint jp = JasperFillManager.fillReport(jr, map, con);
-			        JRXlsxExporter export = new JRXlsxExporter();
-				    export.setExporterInput(new SimpleExporterInput(jp));
-				    export.setExporterOutput(new SimpleOutputStreamExporterOutput(new File(OUT_PUT)));
-				    SimpleXlsxReportConfiguration config = new SimpleXlsxReportConfiguration();
-				    export.setConfiguration(config);
-				    export.exportReport();
-				    JOptionPane.showMessageDialog(mainGui, "<html>The sign-in sheet was successfully created</html>");
-
-				    lbl.setVisible(false);
-				    setVisible(false);
-				    lbl.repaint();
-				    
-				    //Opens file after completed export
-				    OpenFile(OUT_PUT);
-			    } catch (JRException ex) {
-			        ex.printStackTrace();
-			        lbl.setVisible(false);
-			        lbl.repaint();
-			        JOptionPane.showMessageDialog(mainGui, "<html>The sign-in sheet could not be created due to an error:<br>" + ex + "</html>", "Error", JOptionPane.ERROR_MESSAGE);
-			    }
-
-
-			}
-		});
-		contentPane.add(acceptButton);
-
-		JButton button = new JButton("Cancel");
-		button.setBounds(85, 36, 79, 23);
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-			}
-		});
-		contentPane.add(button);
+				JLabel lblPrintSignSheet = new JLabel("Print Sign-In Sheet: ");
+				lblPrintSignSheet.setBounds(33, 4, 131, 14);
+				contentPane.add(lblPrintSignSheet);
+				contentPane.add(acceptButton);
+		
+				JButton button = new JButton("Cancel");
+				button.setBounds(97, 23, 87, 23);
+				button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				contentPane.add(button);
+		
 		
 	
 	}
